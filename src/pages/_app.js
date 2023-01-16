fb0991541ee78a8c78e617b6d1fbb9efb955b489
halilesmer <halil@esmer.de>
2023-01-16 06:47:45 +0100

    🎨 add celcius

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
-import { SWRConfig } from 'swr';
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-    <SWRConfig value={{
-      fetcher : (...args) => fetch(...args).then((res) => res.json())
-    }}>
-
-    <Component {...pageProps} />
-    </SWRConfig>
-  </>
-  )
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/utils/useGeoCode.js
+++ src/utils/useGeoCode.js
@@ -1,14 +1,7 @@
-import React, { useState } from "react";
-
-import useSWR from "swr";
-
-const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-
 export default function useGeoCode() {
- 
+  const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
   const getGeoCodeFunc = async (searchInput) => {
-    console.log("searchInput: ", searchInput);
 
     const endpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput.city}&limit=1&appid=${API_KEY}`;
 
@@ -17,28 +10,22 @@ export default function useGeoCode() {
       const results = await response.json();
 
       return results;
-      setGeoCodeData(results);
-      // return results?.features[0].center;
     } catch (error) {
       console.log("error: ", error);
       return error;
     }
   };
 
-    const foreCastWeather = async (lat, lon) => {
-        console.log("lat: ", lat);
-        console.log("lon: ", lon);
-      const forecastEndpoint =
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${API_KEY}`;
+  const foreCastWeather = async (lat, lon) => {
+    console.log("lat: ", lat);
+    console.log("lon: ", lon);
+    const forecastEndpoint = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&lang=de&units=metric&appid=${API_KEY}`;
 
-      const response = await fetch(forecastEndpoint);
-      const result = await response.json();
+    const response = await fetch(forecastEndpoint);
+    const result = await response.json();
 
-      console.log("foreCastWeather-result: ", result);
-      return result
-    };
-
-
+    return result;
+  };
 
   return { getGeoCodeFunc, foreCastWeather };
 }
